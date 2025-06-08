065ec17b43f60889ca7b3f2f2a4298f9fea57115
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-06-09 00:03:04 +0530

    list, tuple, dict, sets

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
-c 
+# What is the difference between lists, tuples and sets?
+
+# list:
+# A list is collection of data which ordered and changeable( it is mutable)
+
+k=['a', 'c', 'd', 'e']
+print(k)
+print(type(k)) 
+
+# tuple
