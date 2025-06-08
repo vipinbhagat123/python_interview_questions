cff26c37d4356abce7cdbcd58c826ca8ca4af80b
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-06-09 00:06:51 +0530

    list, tuple, set, dict

--- 1.py
+++ 1.py
@@ -8,3 +8,18 @@ print(k)
 print(type(k)) 
 
 # tuple
+# A tuple is a collection of data which ordered and unchangeable( it is immutable)
+
+y=('v', 'e', 1)
+print(y)
+print(type(y))
+
+# set
+# Set is a collection of data which is unordered, unchangeable and unindexed and have unique data.
+m={1,2,2,3,4,5,6}
+print(m)
+
+# dict
+# An unordered mutable collection of key-valuee pairs.
+e={'m':'akd', "adf":234}
+print(e)
